1329aa82209bfb977cabeab68468f975f4781bdd
june276 <138752480+june276@users.noreply.github.com>
2024-04-11 20:29:44 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,13 +1,25 @@
-def f(prev,next,check,n):
-    if check+1==n:
-        return next
-    temp=next
-    next=prev+temp
-    prev=temp
-    check+=1
-    return f(prev,next,check,n)
-n=int(input(""))
-if n<2:
-    print(n)
-else:
-    print(f(0,1,0,n))
+n = int(input())
+
+memo = [0 for i in range(n+1)]
+
+if n>0:
+	memo[1] = 1
+
+
+def fivoMemo(n):
+	if n!=0:
+		n1 = 0
+		n2 = 0
+		if memo[n-1] != 0:
+			n1 = memo[n-1]
+		else:
+			n1 = fivoMemo(n-1)
+		if memo[n-2] != 0:
+			n2 = memo[n-2]
+		else:
+			n2 = fivoMemo(n-2)
+		return n1 + n2
+	else:
+		return 0
+
+print(fivoMemo(n))
